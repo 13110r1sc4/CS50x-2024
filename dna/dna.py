f61fbc808f929b6f495b751b52465ee205ab48e2
13110r1sc4 <147632303+13110r1sc4@users.noreply.github.com>
2024-10-22 15:31:42 +0200

    files

---
+++ dna/dna.py
@@ -0,0 +1,74 @@
+import csv
+import sys
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Missing command-line arguments")
+        sys.exit(1)
+
+    rows = []
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            rows.append(row)
+
+    with open(sys.argv[2]) as file:
+        sequence = file.read()
+
+    keys = list(rows[0].keys())[1:]
+    l_m = []
+
+    for key in keys:
+        l_m.append(str(longest_match(sequence, key)))
+
+    for i in range(len(rows)):
+
+        if l_m == list(rows[i].values())[1:]:
+            print(rows[i]['name'])
+            sys.exit(0)
+
+    print("No match")
+    return
+
+
+def longest_match(sequence, subsequence):
+    """Returns length of longest run of subsequence in sequence."""
+
+    # Initialize variables
+    longest_run = 0
+    subsequence_length = len(subsequence)
+    sequence_length = len(sequence)
+
+    # Check each character in sequence for most consecutive runs of subsequence
+    for i in range(sequence_length):
+
+        # Initialize count of consecutive runs
+        count = 0
+
+        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
+        # If a match, move substring to next potential match in sequence
+        # Continue moving substring and checking for matches until out of consecutive matches
+        while True:
+
+            # Adjust substring start and end
+            start = i + count * subsequence_length
+            end = start + subsequence_length
+
+            # If there is a match in the substring
+            if sequence[start:end] == subsequence:
+                count += 1
+
+            # If there is no match in the substring
+            else:
+                break
+
+        # Update most consecutive matches found
+        longest_run = max(longest_run, count)
+
+    # After checking for runs at each character in seqeuence, return longest run found
+    return longest_run
+
+
+main()
